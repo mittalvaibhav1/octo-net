27ffe47a251dbcc6bff80a78ba9dd55b71431991
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-08 02:40:53 +0530

    Chat component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatHeader from '../ChatHeader/ChatHeader';
+import './Chat.css';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <ChatHeader/>
+        </div>
+    )
+}
+
+export default Chat
