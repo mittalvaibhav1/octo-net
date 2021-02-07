9c4ada5de75cf91e0d60257d1b337171c97d939a
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-08 00:47:11 +0530

    Added pound svg

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -7,7 +7,7 @@ import {
   incrementAsync,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
+import styles from './Counter.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
