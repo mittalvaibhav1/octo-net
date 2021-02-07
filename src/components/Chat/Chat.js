e88c8e990a4a557bc9f7977efd71122aaaab9796
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-08 03:29:43 +0530

    Input section

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ChatHeader from '../ChatHeader/ChatHeader';
 import './Chat.css';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import Message from '../Message/Message';
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader/>
+            <div className="chat__messages">
+                <Message/>
+                <Message/>
+                <Message/>
+            </div>
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large" />
+                <form>
+                    <input type="text" placeholder="Message #music"/>
+                    <button type="submit" className="chat__sendMessage"> Send Message</button>
+                </form>
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large" />
+                    <GifIcon fontSize="large" />
+                    <EmojiEmotionsIcon fontSize="large" />
+                </div>
+            </div>
         </div>
     )
 }
