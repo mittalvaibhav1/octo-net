f358790ca1449d3de9a5e9e2a36928250a269786
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-09 02:03:42 +0530

    firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 import Login from './components/Login/Login';
+import { auth } from './firebase/firebase';
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName
+        }))
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
   return (
     <div className="app">
       { user ? <>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestrore();
+const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
