3341c6ece4af1a6231b5add1f40a4957faec9ff7
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-08 21:46:40 +0530

    Login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import { Button } from '@material-ui/core';
+import React from 'react'
+import './Login.css';
+
+function Login() {
+
+    const signInWithGoogle = () => {
+
+    }
+
+    return (
+        <div className="login">
+            <div className="login__container">
+                <div className="login__logo"></div>
+                <h1 className="login__heading"> OctoNet</h1>
+                <Button  className="login__button" onClick = { signInWithGoogle }> 
+                    <img height="20" src="./google-icon.svg" alt=""/>  Sign In with google
+                </Button>
+                <Button  className="login__button" > 
+                    <img height="20" src="./twitter-logo.png" alt=""/>  Sign In with twitter
+                </Button>
+                <Button  className="login__button" > 
+                    <img height="20" src="./github-icon.png" alt=""/>  Sign In with github
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
