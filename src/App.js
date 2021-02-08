dc8cdba82236ac00e577ea9c5ee05c7cd6c15eaa
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-08 19:02:33 +0530

    configuring redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
---
+++ src/features/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user += action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+// export const incrementAsync = amount => dispatch => {
+//   setTimeout(() => {
+//     dispatch(incrementByAmount(amount));
+//   }, 1000);
+// };
+
+
+export const selectUser = state => state.user.user;
+
+export default userSlice.reducer;
