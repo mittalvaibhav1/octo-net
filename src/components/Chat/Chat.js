314eab826f26a8544456d0e5197c0e8763428287
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-09 20:37:27 +0530

    Chat

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ChatHeader from '../ChatHeader/ChatHeader';
 import './Chat.css';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
@@ -6,18 +6,67 @@ import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 import GifIcon from '@material-ui/icons/Gif';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import Message from '../Message/Message';
+import { useSelector } from 'react-redux';
+import { selectChannelId, selectChannelName } from '../../features/appSlice';
+import { selectUser } from '../../features/userSlice';
+import db from '../../firebase/firebase';
+import firebase from 'firebase';
+import notificationSound from '../../sounds/micSound.mp3'
 
 function Chat() {
+
+    const channelName = useSelector(selectChannelName);
+    const channelId = useSelector(selectChannelId);
+    const user = useSelector(selectUser);
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([]);
+    const notificationAudio = new Audio(notificationSound);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        notificationAudio.play(); 
+        db.collection('textChannels').doc(channelId)
+        .collection("messages").add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            user: user,
+            message: input
+        });
+        setInput("");
+    }
+
+    useEffect(() => {
+        if(channelId) {
+            db.collection('textChannels').doc(channelId)
+            .collection("messages").orderBy('timestamp')
+            .onSnapshot((snapshot) => {
+                setMessages(snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    message: doc.data()
+                })));
+            })
+        }
+        
+    }, [channelId]);
+
     return (
         <div className="chat">
-            <ChatHeader/>
+            <ChatHeader channelName = { channelName } />
             <div className="chat__messages">
-                <Message/>
+                {
+                    messages.map(({id, message}) => (
+                        <Message 
+                            key = { id }
+                            timestamp = { message.timestamp }
+                            message = { message.message }
+                            user = { message.user }
+                        />
+                    )) 
+                }
             </div>
             <div className="chat__input">
                 <AddCircleIcon fontSize="large" />
-                <form>
-                    <input type="text" placeholder="Message #music"/>
+                <form onSubmit={ handleSubmit } >
+                    <input disabled = { !channelId } value={ input } onChange={ e => setInput(e.target.value) } type="text" placeholder="Message #music"/>
                     <button type="submit" className="chat__sendMessage"> Send Message</button>
                 </form>
                 <div className="chat__inputIcons">
--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -5,17 +5,27 @@ export const appSlice = createSlice({
     initialState: {
         channelId: null,
         channelName: null,
+        voiceChannelId: null,
+        voiceChannelName: null,
     },
     reducers: {
-        setChannelId : (state, action) => {
-            state.app += action.payload;
+        setChannelInfo : (state, action) => {
+            state.channelId = action.payload.channelId;
+            state.channelName = action.payload.channelName;
+        },
+        setVoiceChannelInfo: (state, action) => {
+            state.voiceChannelId = action.payload.channelId;
+            state.voiceChannelName = action.payload.channelName;
         },
     },
 });
 
-export const { setChannelId } = appSlice.actions;
+export const { setChannelInfo, setVoiceChannelInfo } = appSlice.actions;
 
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
+
+export const selectVoiceChannelId = (state) => state.app.voiceChannelId;
+export const selectVoiceChannelName = (state) => state.app.voiceChannelName;
 
 export default appSlice.reducer;
