46fd27802d789f7a269d846de2342660ce9e9f7c
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-09 20:36:54 +0530

    github and twitter

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,26 @@
 import { Button } from '@material-ui/core';
 import React from 'react'
 import './Login.css';
-import { auth, provider } from '../../firebase/firebase';
+import { auth, githubAuthProvider, googleAuthProvider, twitterAuthProvider } from '../../firebase/firebase';
 
 function Login() {
 
     const signInWithGoogle = () => {
-        auth.signInWithPopup(provider)
+        auth.signInWithPopup(googleAuthProvider)
+        .catch((error) => {
+            console.log('Error: ' + error.message);
+        })
+    }
+
+    const signInWithTwitter = () => {
+        auth.signInWithPopup(twitterAuthProvider)
+        .catch((error) => {
+            console.log('Error: ' + error.message);
+        })
+    }
+
+    const signInWithGithub = () => {
+        auth.signInWithPopup(githubAuthProvider)
         .catch((error) => {
             console.log('Error: ' + error.message);
         })
@@ -20,10 +34,10 @@ function Login() {
                 <Button  className="login__button" onClick = { signInWithGoogle }> 
                     <img height="20" src="./google-icon.svg" alt=""/>  Sign In with google
                 </Button>
-                <Button  className="login__button" > 
+                <Button  className="login__button"onClick = { signInWithTwitter } > 
                     <img height="20" src="./twitter-logo.png" alt=""/>  Sign In with twitter
                 </Button>
-                <Button  className="login__button" > 
+                <Button  className="login__button" onClick = { signInWithGithub } > 
                     <img height="20" src="./github-icon.png" alt=""/>  Sign In with github
                 </Button>
             </div>
