468a9517e5ddbfc8656d2ecb1d6791aaea4f3b3c
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-09 20:37:05 +0530

    optimaization

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,17 +1,20 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Message.css';
+import { formatRelative} from 'date-fns';
 
-function Message() {
+function Message({ user, message, timestamp }) {
     return (
         <div className="message">
-            <Avatar  src="./IronMan.jpg" />
+            <Avatar  src={ user.photo } />
             <div className="message__info">
                 <h4>
-                    Dean Winchester
-                    <span className="message__timestamp">Today at 2:17 AM</span>
+                    { user.displayName }
+                    <span className="message__timestamp">
+                        { timestamp && formatRelative(new Date(timestamp?.toDate()), new Date()) }
+                    </span>
                 </h4>
-                <p>Hey! I'm watching Brookyln 99 rn.</p>
+                <p>{ message }</p>
             </div>
         </div>
     )
