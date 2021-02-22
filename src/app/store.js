0025c64c2dfa25959637b48e2800e1540b4ed406
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-23 00:46:59 +0530

    middleware changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 import appReducer from '../features/appSlice';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+  immutableCheck: false
+})
 
 export default configureStore({
   reducer: {
     user: userReducer,
     app: appReducer,
   },
+  middleware: customizedMiddleware
 });
