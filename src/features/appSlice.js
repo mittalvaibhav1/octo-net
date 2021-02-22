7bc308e2dc454d765643dcff9893735646f41e01
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-02-23 00:50:48 +0530

    added support for video channels

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -7,6 +7,9 @@ export const appSlice = createSlice({
         channelName: null,
         voiceChannelId: null,
         voiceChannelName: null,
+        videoChannelId: null,
+        videoChannelName: null,
+        peerVideos: []
     },
     reducers: {
         setChannelInfo : (state, action) => {
@@ -17,15 +20,28 @@ export const appSlice = createSlice({
             state.voiceChannelId = action.payload.channelId;
             state.voiceChannelName = action.payload.channelName;
         },
+        setVideoChannelInfo: (state, action) => {
+            state.videoChannelId = action.payload.channelId;
+            state.videoChannelName = action.payload.channelName;
+        },
+        setPeerVideos: (state, action) => {
+            state.peerVideos = action.payload.peerVideos;
+        }
     },
 });
 
-export const { setChannelInfo, setVoiceChannelInfo } = appSlice.actions;
+export const { setChannelInfo, setVoiceChannelInfo, setVideoChannelInfo, setPeerVideos } = appSlice.actions;
 
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
 
 export const selectVoiceChannelId = (state) => state.app.voiceChannelId;
 export const selectVoiceChannelName = (state) => state.app.voiceChannelName;
+
+
+export const selectVideoChannelId = (state) => state.app.videoChannelId;
+export const selectVideoChannelName = (state) => state.app.videoChannelName;
+
+export const selectPeerVideos = (state) => state.app.peerVideos;
 
 export default appSlice.reducer;
