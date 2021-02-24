201d2ccab409e64e2be7df43340f95937d0dfb68
Vaibhav Mittal <53215737+mittalvaibhav1@users.noreply.github.com>
2021-02-24 13:25:33 +0530

    firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCKaW4Bigj48hmNLqOHUEYtNp7Ys1I5Eh8",
-    authDomain: "octo-net.firebaseapp.com",
-    projectId: "octo-net",
-    storageBucket: "octo-net.appspot.com",
-    messagingSenderId: "87652945238",
-    appId: "1:87652945238:web:cd9118ff7ed954501b1af7",
-    measurementId: "G-4SDRW6JPW2"
+   "YOUR FIREBASE CONFIG HERE"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
